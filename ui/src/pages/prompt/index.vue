9c98758fb93ef6cd04bc804d20f179ea7dfac18f
GivemeYuluJia <1095778225@qq.com>
2023-05-30 14:56:14 +0800

    feat: add Prompt Page

---
+++ ui/src/pages/prompt/index.vue
@@ -0,0 +1,489 @@
+<template>
+  <div class="prompt-container">
+    <main>
+      <div class="prompt-box-wrapper" :key="prompt.key" v-for="(prompt, index) in promptTypeList">
+        <a-textarea
+          :value="prompt.word"
+          class="prompt-textarea"
+          :data-type="prompt.key"
+          :placeholder="prompt.placeholder" 
+          allow-clear 
+          show-count
+          :bordered="false" 
+          :maxlength="promptWordMaxLength"
+          :rows="6"
+          @change="handleChangeWords($event, index)"
+          @keydown.enter.native="(e) => e.preventDefault()"
+          @keyup.enter.native="handleTranslateWords($event, index)"
+        />
+        <div style="color:#fff">{{ translateStatus.Forward }}</div>
+        <div class="operate-box">
+          <div class="operate-top-inline">
+            <div :key="i" v-for="(o, i) in operateList" class="operate-item">
+              <div class="label" v-if="o.name === 'switch'">{{ o.title }}</div>
+               <template v-if="o.name === 'switch'">
+                  <a-switch :checked="prompt.syncCheck" @change="(v) => { prompt.syncCheck = v; }"></a-switch>
+               </template>
+               <template v-else-if="o.name === 'button'">
+                  <a-button 
+                    type="primary" 
+                    ghost 
+                    @click="handleActionClick(o.methods, prompt.key)"
+                  >
+                    {{ o.title }}
+                  </a-button>
+               </template>
+            </div>
+          </div>
+          <template v-for="p in promptTypeList">
+            <DragList 
+              :key="p.key"
+              v-if="p.key === prompt.key"
+              :list="getCurrentPromptList(p.key)"
+            >
+              <template #list-body="scope">
+                <template v-if="p.key === 'Forward'">
+                  <DragPreview
+                    :key="t.id"
+                    v-for="(t, i) in scope.list.value"
+                    v-model:dragIndex="currentDragIndex"
+                    v-model:dragType="currentDragType"
+                    v-model:list="translateForwardList"
+                    :listType="p.key"
+                    :preview="t"
+                    :index="i"
+                  />
+                </template>
+                <template v-else-if="p.key === 'Negative'">
+                  <DragPreview
+                    :key="t.id"
+                    v-for="(t, i) in scope.list.value"
+                    v-model:dragIndex="currentDragIndex"
+                    v-model:dragType="currentDragType"
+                    v-model:list="translateNegativeList"
+                    :listType="p.key"
+                    :preview="t"
+                    :index="i"
+                  />
+                </template>
+              </template>
+            </DragList>
+          </template>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed, ref, reactive, Ref, watch } from 'vue';
+import { message } from 'ant-design-vue';
+import { translationPromptRequest } from '../../api/index';
+import DragList from '../../components/DragList/index.vue';
+import DragPreview from '../../components/DragPreview/index.vue';
+import { el_selector } from "../../utils/gradio";
+import {
+  combinationPrompt,
+  countLayers,
+  canTranslate, 
+  getPromptWeightNum, 
+  loraRegex,
+  splitTag, 
+  splitPrompts
+} from "../../utils/common";
+
+type promptType = 'Forward' | 'Negative';
+type operateMethods = 'copyByChinese' | 'copyByEnglish' | 'sort';
+interface promptItemType {
+  key: promptType,
+  title: string,
+  placeholder: string,
+  word: string,
+  syncCheck: boolean,
+}
+interface translateType {
+  id: number,
+  chineseName: string,
+  englishName: string,
+  sourceTextType: string,
+  weight: number,
+  type: '' | 'lora'
+}
+type translateStatusType = Record<promptType, 'first' | 'update'>;
+
+const idByReal: Ref<number> = ref(0);
+const currentDragType: Ref<promptType | ''> = ref('');
+const currentDragIndex: Ref<Number> = ref(0);
+const promptTypeList: Array<promptItemType> = reactive([
+  {
+    key: 'Forward',
+    title: '正向提示词',
+    placeholder: '请输入正向提示词',
+    word: '',
+    syncCheck: true,
+  },
+  {
+    key: 'Negative',
+    title: '反向提示词',
+    placeholder: '请输入反向提示词',
+    word: '',
+    syncCheck: true,
+  }
+]);
+const operateList = reactive([
+  {
+    name: 'switch',
+    title: '同步',
+    forwardValue: true,
+    negativeValue: true
+  },
+  {
+    name: 'button',
+    title: '复制中文',
+    methods: 'copyByChinese'
+  },
+  {
+    name: 'button',
+    title: '复制英文',
+    methods: 'copyByEnglish'
+  },
+  {
+    name: 'button',
+    title: '排序',
+    methods: 'sort'
+  }
+])
+const promptWordMaxLength = ref(300);
+const translateForwardList: Ref<Array<translateType>> = ref([]);
+const translateNegativeList: Ref<Array<translateType>> = ref([]);
+const translateStatus: Ref<translateStatusType> = ref({
+  Forward: 'first',
+  Negative: 'first'
+})
+
+const copyForwardPromptByCN = computed(() => 
+  translateForwardList.value.map(p => 
+    combinationPrompt(p.chineseName, p.weight, p.type)
+  ).join(',')
+);
+const copyForwardPromptByEN = computed(() => 
+  translateForwardList.value.map(p => 
+    combinationPrompt(p.englishName, p.weight, p.type)
+  ).join(',')
+);
+const copyNegativePromptByCN = computed(() => 
+  translateNegativeList.value.map(p => 
+    combinationPrompt(p.chineseName, p.weight, p.type)
+  ).join(',')
+);
+const copyNegativePromptByEN = computed(() => 
+  translateNegativeList.value.map(p => 
+    combinationPrompt(p.englishName, p.weight, p.type)
+  ).join(',')
+);
+
+watch(
+  translateForwardList,
+  (count, prevCount) => {
+    const { syncCheck } = promptTypeList[0]; 
+    if (syncCheck) {
+      if (translateStatus.value.Forward === 'update') {
+        promptTypeList[0].word = copyForwardPromptByEN.value;
+      } else if (translateStatus.value.Forward === 'first') {
+        translateStatus.value.Forward = 'update';
+      }
+    }
+  },
+  { deep: true }
+)
+watch(
+  translateNegativeList,
+  (count, prevCount) => {
+    const { syncCheck } = promptTypeList[1]; 
+    if (syncCheck) {
+      if (translateStatus.value.Negative === 'update') {
+        promptTypeList[1].word = copyNegativePromptByEN.value;
+      } else if (translateStatus.value.Negative === 'first') {
+        translateStatus.value.Negative = 'update';
+      }
+    }
+  },
+  { deep: true }
+)
+
+/**
+ *  复制函数
+ *  @param {string} text
+ *  @returns {promise}
+ */
+const copyText = (text: string) => {
+  return navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+};
+
+const handleActionClick = (methods: operateMethods, type: promptType) => {
+  switch (methods) {
+    case 'copyByChinese':
+      const res = (type === 'Forward' ? copyForwardPromptByCN : copyNegativePromptByCN).value;
+      copyText(res).then((bool) => {
+        if (bool) {
+          message.success('复制成功：'+ res);
+        } else {
+          message.error('复制失败');
+        }
+      })
+      break;
+    case 'copyByEnglish':
+      const resByEN = (type === 'Forward' ? copyForwardPromptByEN : copyNegativePromptByEN).value;
+      copyText(resByEN).then((bool) => {
+        if (bool) {
+          message.success('复制成功：'+ resByEN);
+        } else {
+          message.error('复制失败');
+        }
+      })
+      break;
+    case 'sort':
+      handleSortPromptList(type)
+      break;
+    default:
+      break;
+  }
+};
+
+/**
+ *  提示词列表排序操作
+ *  @param {promptType} type
+ */
+const handleSortPromptList = (type: promptType) => {
+  const list = getCurrentPromptList(type);
+  list.value.sort((a, b) => {
+    if (a.type === 'lora' && b.type === 'lora') {
+      return Math.abs(b.weight) - Math.abs(a.weight);
+    } else if (a.type === 'lora' && b.type !== 'lora') {
+      return -1;
+    } else if (a.type !== 'lora' && b.type === 'lora') {
+      return 1;
+    }
+    if (b.weight === a.weight) {
+      return b.chineseName > a.chineseName ? 1 : a.chineseName > b.chineseName ? -1 : 0;
+    }
+    return b.weight - a.weight;
+  });
+}
+
+const getCurrentPromptList = (type: promptType) => {
+  return type === 'Forward' ? translateForwardList : translateNegativeList;
+};
+
+const handleChangeWords = async(e: any, index: number) => {
+  promptTypeList[index].word = e.target.value;
+};
+
+/**
+ *  翻译词条操作
+ *  @param {KeyboardEvent | FocusEvent} e
+ *  @param {number} index
+ */
+const handleTranslateWords = async (e: KeyboardEvent | FocusEvent, index: number) => {
+  const { word, key } = promptTypeList[index];
+  // 换行
+  if (e instanceof KeyboardEvent && e.shiftKey && e.key === 'Enter') {
+    promptTypeList[index].word = word + '\n';
+    return;
+  }
+  e.preventDefault();
+  const wordArr = splitPrompts(word);
+  if (!wordArr.length) return;
+  const translateWords = [];
+  const cacheWords = wordArr.map((word, index) => {
+    let type = loraRegex.test(word) ? 'lora' : '';
+    const { value, left, right } = splitTag(word.trim());
+    let isCanTranslate = canTranslate(word);
+    const wordName = value;
+    if (isCanTranslate) {
+      translateWords.push('(' + wordName + ')');
+    }
+    let weight = getPromptWeightNum(word);
+    let layer = 0;
+    if (!weight) {
+      if (left && left[0] === '(' && right && right[right.length - 1] === ')') {
+        layer = countLayers(left + right, '(', ')');
+      } else if (left && left[0] === '[' && right && right[right.length - 1] === ']') {
+        layer = countLayers(left + right, '(', ')');
+      }
+    }
+    layer && (weight = Number(String(Math.pow(1.1, layer)).replace(/^(.*\..{1}).*$/, '$1')));
+    return {
+      name: wordName,
+      weight,
+      index,
+      isCanTranslate,
+      type
+    }
+  });
+  const res = await reqTranslate(translateWords.join(','));
+  if (res && res.code === 0) {
+    const newData = res.data;
+    cacheWords.map(({name, weight, index, isCanTranslate, type}) => {
+      // 不能翻译的词条插入接口返回队列中
+      if (!isCanTranslate) {
+        newData.splice(index, 0, {
+          chineseName: name,
+          englishName: name,
+          id: idByReal.value++,
+          sourceTextType: "en",
+          type,
+          weight,
+        });
+        return;
+      }
+      newData[index].type =  type;
+      newData[index].weight =  weight;
+    });
+    const cur = getCurrentPromptList(key);
+    cur.value = newData;
+  }
+};
+
+/**
+ *  请求翻译
+ *  @param {string} words
+ *  @returns {object}
+ */
+const reqTranslate = async (words: string) => {
+  if (!words.length) return;
+  try {
+    const res: any = await translationPromptRequest({
+      text: words
+    });
+    return res;
+  } catch (error) {
+    console.log(error);
+    return { msg: error };
+  }
+};
+</script>
+<style scoped lang="less">
+.prompt-container {
+  width: 100%;
+  height: 100vh;
+  overflow-y: auto;
+  background-color: #121212;
+  > main {
+    width: 100%;
+    max-width: 1280px;
+    height: 100vh;
+    min-height: 100%;
+    margin: 0 auto;
+    background-color: initial;
+    padding: 1rem 0.75rem;
+    box-sizing: border-box;
+  }
+  .prompt-box-wrapper {
+    margin-top: 0.75rem;
+  }
+  .prompt-textarea {
+    background-color: #232323;
+    margin-bottom: 1.875rem;
+    :deep(.ant-input) {
+      color: #fff;
+      height: 142px !important;
+      max-height: 142px !important;
+    }
+    &::after {
+      color: #fff;
+    }
+  }
+  .operate-box {
+    border: 1px solid #3d3d3d;
+  }
+  .operate-top-inline {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0.75rem;
+    border-bottom: 1px solid #3d3d3d;
+    > div {
+      display: flex;
+      align-items: center;
+      margin-bottom: 0.375rem;
+    }
+    > div + div {
+      margin-left: 0.75rem;
+    }
+    .label {
+      font-size: 1rem;
+      font-weight: 500;
+      color: #fff;
+      margin-right: 0.5rem;
+    }
+    :deep(.ant-switch) {
+      background-color: #3d3d3d;
+    }
+    :deep(.ant-switch-checked) {
+      background-color: #1890ff;
+    }
+  }
+
+  // .prompt-item {
+  //   border: 1px solid transparent;
+  //   border-radius: 0.375rem;
+  //   overflow: hidden;
+  // }
+  // .prompt-item-child {
+  //   position: relative;
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: space-between;
+  //   box-sizing: border-box;
+  //   height: 3.25rem;
+  //   line-height: 2.5rem;
+  //   padding: 0.375rem 0.75rem;
+  //   cursor: move;
+  //   .text {
+  //     font-size: 1rem;
+  //     font-weight: 500;
+  //     color: #fff;
+  //     width: 100%;
+  //     white-space: nowrap;
+  //     overflow: hidden;
+  //     text-overflow: ellipsis;
+  //     margin-right: 0.375rem;
+  //   }
+  //   .close-btn {
+  //     font-size: 1rem;
+  //     text-align: center;
+  //     flex-shrink: 0;
+  //     color: #fff;
+  //   }
+  // }
+}
+// .prompt-item-popover {
+//   display: flex;
+//   align-items: center;
+//   .weight,
+//   .text {
+//     font-size: 0.875rem;
+//     font-weight: 500;
+//     color: #fff;
+//   }
+//   .weight {
+//     padding: 0 0.375rem;
+//   }
+//   .text {
+//     margin-left: 0.65rem;
+//   }
+// }
+// :global(.ant-popover-inner) {
+//   background-color: #232323;
+//   border: 1px solid #3d3d3d;
+// }
+// :global(.ant-popover-arrow-content) {
+//   background-color:#3d3d3d;
+// }
+</style>
---
+++ ui/src/utils/common.js
@@ -0,0 +1,334 @@
+export const loraRegex = /^\<lora:\s*([^\:]+)\s*(:)?\s*([0-9\.]+)?\>$/;
+export const lycoRegex = /^\<lyco:\s*([^\:]+)\s*(:)?\s*([0-9\.]+)?\>$/;
+export const weightNumRegex = /(.*):([0-9\.]+)/;
+export const weightNumRegexEN = /(.*):\s*([0-9\.]+)/;
+export const weightNumRegexCN = /(.*)：\s*([0-9\.]+)/;
+export const bracketsEN = [
+  { '(': '(', ')': ')' },
+  { '[': '[', ']': ']' },
+  { '{': '{', '}': '}' },
+  { '<': '<', '>': '>' },
+];
+export const bracketsCN = [
+  { '（': '(', '）': ')' },
+  { '【': '[', '】': ']' },
+  { '《': '<', '》': '>' },
+  { '「': '{', '」': '}' },
+  { '『': '{', '』': '}' },
+  { '〈': '<', '〉': '>' },
+  { '﹝': '(', '﹞': ')' },
+  { '﹛': '{', '﹜': '}' },
+  { '﹙': '(', '﹚': ')' },
+];
+
+/**
+ * 替换标签
+ * @param text {string}
+ * @returns {*|string}
+ */
+export const replaceTag = (text) => {
+  if (typeof text !== "string") return text
+  if (text === "") return text
+  text = replaceBrackets(text)
+  if (weightNumRegexEN.test(text)) text = text.replace(weightNumRegexEN, '$1:$2')
+  if (weightNumRegexCN.test(text)) text = text.replace(weightNumRegexCN, '$1:$2')
+  return text
+};
+
+/**
+ * 替换括号
+ * @param text
+ * @returns {*}
+ */
+export const replaceBrackets = (text) => {
+  const length = text.length
+  if (length === 0) return text
+  let replaces = {}
+  bracketsCN.forEach(item => {
+    for (const key in item) {
+      replaces[key] = item[key]
+    }
+  })
+
+  let start = text[0]
+  let end = text[length - 1]
+  if (typeof replaces[start] !== "undefined") {
+    text = replaces[start] + text.substring(1)
+  }
+  if (typeof replaces[end] !== "undefined") {
+    text = text.substring(0, length - 1) + replaces[end]
+  }
+  return text
+};
+
+/**
+ * 是否有括号
+ * @param text {string}
+ * @returns {Array|boolean}
+ */
+export const hasBrackets = (text) => {
+  const length = text.length
+  if (length === 0) return false
+  let brackets = []
+  bracketsEN.forEach(item => {
+    let temp = []
+    for (const key in item) {
+      temp.push(key)
+    }
+    brackets.push(temp)
+  })
+  bracketsCN.forEach(item => {
+    let temp = []
+    for (const key in item) {
+      temp.push(key)
+    }
+    brackets.push(temp)
+  })
+
+  let start = text[0]
+  let end = text[length - 1]
+  for (const bracket of brackets) {
+    if (bracket[0] === start && bracket[1] === end) {
+      return bracket
+    }
+  }
+  return false
+};
+
+/**
+ * 拆分标签
+ * @param tag {string}
+ * @returns {{left: string, right: string, value: string}}
+ */
+export const splitTag = (tag) => {
+  let result = { left: '', value: '', right: '' }
+  let match = tag.match(/^([\(\<\{\[]+)(.*)$/)
+  if (!match) {
+    // 没有匹配到左括号
+    result.value = tag
+    return result
+  }
+  result.left = match[1]
+  tag = match[2]
+  match = tag.match(/((\:[0-9\.]+)?[\)\>\}\]]+)$/)
+  if (!match) {
+    // 没有匹配到右括号
+    result.value = tag
+    return result
+  }
+  result.right = match[1]
+  tag = tag.substring(0, tag.length - result.right.length)
+  result.value = tag
+  return result
+};
+
+/**
+ * 分割标签
+ * @param tags {string}
+ * @returns {string[]}
+ */
+export const splitTags = (tags) => {
+  if (tags === null || tags === undefined || tags === false || tags === "" || tags.trim() === "") return []
+
+  tags = tags.replace(/，/g, ',') // 中文逗号
+  tags = tags.replace(/。/g, ',') // 中文句号
+  tags = tags.replace(/、/g, ',') // 中文顿号
+  tags = tags.replace(/；/g, ',') // 中文分号
+  tags = tags.replace(/．/g, ',') // 日文句号
+
+  tags = tags.replace(/\t/g, '\n') // 制表符
+  tags = tags.replace(/\r/g, '\n') // 回车符
+  tags = tags.replace(/\n+/g, '\n') // 连续换行符
+
+  const brackets = {
+      '(': ')',
+      '[': ']',
+      '<': '>',
+      '{': '}'
+  }
+  const bracketStarts = Object.keys(brackets)
+
+  let length = tags.length
+  let temp = ''
+  let startBracketChar = ''
+  let endBracketChar = ''
+  let bracketCount = 0
+  let result = []
+  for (let i = 0; i < length; i++) {
+      const char = tags[i]
+      if (char === "\n") {
+          if (startBracketChar === '') {
+              // 前面没有括号
+              if (temp.trim() !== "") {
+                  result.push(temp.trim())
+              }
+              result.push("\n")
+              bracketCount = 0
+              startBracketChar = ''
+              endBracketChar = ''
+              temp = ''
+          } else {
+              // 前面有括号
+              temp += ' '
+          }
+      } else if (char === ",") {
+          if (startBracketChar === '') {
+              // 前面没有括号
+              result.push(temp.trim())
+              bracketCount = 0
+              startBracketChar = ''
+              endBracketChar = ''
+              temp = ''
+          } else {
+              // 前面有括号
+              temp += char
+          }
+      } else {
+          if (startBracketChar === '') {
+              // 前面没有括号
+              if (bracketStarts.includes(char)) {
+                  // 括号开始
+                  bracketCount = 1
+                  startBracketChar = char
+                  endBracketChar = brackets[char]
+                  temp += char
+              } else {
+                  temp += char
+              }
+          } else {
+              // 前面有括号
+              if (char === endBracketChar) {
+                  // 是结束括号的标识，减掉括号计数
+                  bracketCount--
+                  if (bracketCount === 0) {
+                      // 括号计数为0，括号结束
+                      startBracketChar = ''
+                      endBracketChar = ''
+                      temp += char
+                  } else {
+                      temp += char
+                  }
+              } else if (char === startBracketChar) {
+                  // 是开始括号的标识，加上括号计数
+                  bracketCount++
+                  temp += char
+              } else {
+                  temp += char
+              }
+          }
+      }
+  }
+  if (temp !== '') {
+      result.push(temp.trim())
+  }
+  return result
+};
+
+/**
+ * 分割标签-正则
+ * @param tags {string}
+ * @returns {string[]}
+ */
+export const splitPrompts = (prompts) => {
+  if (
+    prompts === null ||
+    prompts === undefined ||
+    prompts === false ||
+    prompts === "" ||
+    prompts.trim() === ""
+  )
+    return [];
+
+  prompts = prompts.replace(/，/g, ","); // 中文逗号
+  prompts = prompts.replace(/。/g, ","); // 中文句号
+  prompts = prompts.replace(/、/g, ","); // 中文顿号
+  prompts = prompts.replace(/；/g, ","); // 中文分号
+  prompts = prompts.replace(/．/g, ","); // 日文句号
+
+  prompts = prompts.replace(/\t/g, "\n"); // 制表符
+  prompts = prompts.replace(/\r/g, "\n"); // 回车符
+  prompts = prompts.replace(/\n+/g, "\n"); // 连续换行符
+  return prompts.split(/,(?![^(^[]*?(\)|\]))/).filter(p => !!p);
+}
+
+/**
+ * 是否可以翻译
+ * @param text {string}
+ * @returns {boolean}
+ */
+export const canTranslate = (text) => {
+    // 如果为空，不翻译
+    if (text.trim() === '') return false
+    // 如果<>包裹，不翻译
+    if (text[0] === '<' && text[text.length - 1] === '>') return false
+    // 如果是数字、标点符号，不翻译
+    const regex = /^[0-9`~!@#$%^&*()_+\-=\[\]{}\\|;:'",.\/<>?]+$/
+    if (regex.test(text)) return false
+    // 如果是单个英文字母，不翻译
+    if (/^[a-zA-Z]$/.test(text)) return false
+    return true
+};
+
+/**
+ * 获取标签的权重数
+ * @param prompt {string}
+ * @returns {number}
+ */
+export const getPromptWeightNum = (prompt) => {
+  const match = prompt.match(weightNumRegex);
+  let weightNum = match ? parseFloat(match[2]) : 0;
+  weightNum = weightNum >= 0 ? weightNum : 0;
+  return weightNum;
+};
+
+/**
+ * 计算字符串包裹的层数
+ * @param str {string}
+ * @param start {string}
+ * @param end {string}
+ * @returns {number}
+ */
+export const countLayers = (str, start = '(', end = ')') => {
+    let count = 0
+    if (str.length < 2) return count // 长度小于2，不可能有会有包裹
+    while (true) {
+        // 取出第一个和最后一个字符
+        let first = str[0]
+        let last = str[str.length - 1]
+        if (first === start && last === end) {
+            // 如果第一个和最后一个字符是括号，且是对应的括号，那么层数加1，然后去掉第一个和最后一个字符
+            count++
+            // 去掉第一个和最后一个字符
+            str = str.slice(1, str.length - 1)
+        } else {
+            break
+        }
+    }
+    return count
+};
+
+export const combinationPrompt = (str, weight, type = '') => {
+  let res = str;
+  if (!type && weight) {
+    res = "(" + str + ":" + weight + ")";
+  } else if (type === "lora") {
+    res = "<" + str + ":" + weight + ">";
+  };
+  return res;
+};
+
+export const addFloat = (arg1, arg2) => {
+  let r1, r2, m;
+  try {
+    r1 = arg1.toString().split('.')[1].length;
+  } catch (e) {
+    r1 = 0;
+  }
+  try {
+    r2 = arg2.toString().split('.')[1].length;
+  } catch (e) {
+    r2 = 0;
+  }
+  m = Math.pow(10, Math.max(r1, r2));
+  return (parseInt(arg1 * m) + parseInt(arg2 * m)) / m;
+};

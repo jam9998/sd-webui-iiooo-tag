1af2e9133a855ee8c7f9b076d0828e5fd534cdb3
EVAL\eval <lihaibo123as@163.com>
2023-06-08 19:59:52 +0800

    entry el selector

--- ui/src/utils/gradio.js
+++ ui/src/utils/gradio.js
@@ -18,16 +18,14 @@ let el_selector = {
   el_prompt_textarea_forward: ".prompt-textarea textarea[data-type=Forward]",
   // 反向提示词文本域
   el_prompt_textarea_negative: ".prompt-textarea textarea[data-type=Negative]",
-  el_txt2img_prompt: "div[id=txt2img_prompt_container] div",
-  el_txt2img_prompt_textarea:
-    "div[id=txt2img_prompt] textarea[data-testid=textbox]",
-  el_txt2img_neg_prompt_textarea:
-    "div[id=txt2img_neg_prompt] textarea[data-testid=textbox]",
-  el_img2img_prompt: "div[id=img2img_prompt_container] div",
-  el_img2img_prompt_textarea:
-    "div[id=img2img_prompt] textarea[data-testid=textbox]",
-  el_img2img_neg_prompt_textarea:
-    "div[id=img2img_neg_prompt] textarea[data-testid=textbox]",
+
+  el_txt2img_prompt: "div[id=txt2img_prompt_container] > div[id^=component]",
+  el_txt2img_prompt_textarea: "div[id=txt2img_prompt] textarea[data-testid=textbox]",
+  el_txt2img_neg_prompt_textarea: "div[id=txt2img_neg_prompt] textarea[data-testid=textbox]",
+  
+  el_img2img_prompt: "div[id=img2img_prompt_container] > div[id^=component]",
+  el_img2img_prompt_textarea: "div[id=img2img_prompt] textarea[data-testid=textbox]",
+  el_img2img_neg_prompt_textarea: "div[id=img2img_neg_prompt] textarea[data-testid=textbox]",
 };
 
 export { el_selector } 
